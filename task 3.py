f33b844b4547a1011f5ed3b1e17790eb58fc0d43
R.kishore kumar <kishorekumar46528@gmail.com>
2026-08-19 18:17:41 +0530

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,50 @@
+
+#1
+for i in range(1,11):
+    print(i)
+
+#2
+for i in range(10,0,-1):
+    print(i)
+
+#3
+for i in range(1,21):
+    if i%2==0:
+        print("even number:",i)
+
+#4
+for i in range(1,21):
+    if i%2!=0:
+        print("odd number:",i)
+
+#5
+num=5
+for i in range(1,11):
+    print("multiple:",num*i)
+
+
+#6
+sum=0
+for i in range(1,101):
+    sum=sum+i
+    print("sum=",sum)
+
+#7
+sum=0
+for i in range(1,51):
+    if i%2==0:
+        sum=sum+i
+        print("sum=",sum)
+
+
+#8
+A="PYTHON"
+for i in A:
+    print(i)
+
+#9
+num=0
+for i in range(1,11):
+    num=i*2
+    print("number:",num)
+
